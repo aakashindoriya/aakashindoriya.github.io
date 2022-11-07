c70fed9a96705a1f0d2cd2bd232cfd4126fdfb08
aakash <37771235+aakashindoriya@users.noreply.github.com>
2022-11-07 19:11:08 +0530

    done

--- src/components/gihubCalender.jsx
+++ src/components/gihubCalender.jsx
@@ -1,24 +1,28 @@
-import { Box, Image, Text } from "@chakra-ui/react";
+import { Box, Flex, Image, Text, useMediaQuery } from "@chakra-ui/react";
 import { useContext } from "react";
 import GitHubCalendar from "react-github-calendar";
 import { Theme } from "./thems";
 
 export default function GithubCalender(){
-   
+    const [isMobile] = useMediaQuery("(max-width: 768px)") 
 let {theme} =useContext(Theme)
     return(
         <Box bg={theme.bgColor} color={theme.textcolor} border="1px solid transparent" >
             <Text ml="10%" mt={50} mb={10} as="b" fontSize={"30px"}>GitHub Calendar</Text>
             <Box  display="grid" justifyContent={"center"} p={15} mt={20}>
           
-         <Box> <GitHubCalendar username="aakashindoriya" 
+         <Box w="90%" m="auto"> <GitHubCalendar username="aakashindoriya" 
         blockSize={19}
         blockMargin={7}
         fontSize={21}
         
         /></Box> 
         <Text ml="10%" mt={50} mb={10} as="b" fontSize={"30px"}>GitHub Graph</Text>
-        <Image src={`https://activity-graph.herokuapp.com/graph?username=aakashindoriya&custom_title=Aakash%20Indoriya%27s%20Contribution%20Graph&theme=gruvbox&bg_color=${theme.bgColor}&hide_border=true&line=d1a01f&point=c58545`} />
+        <Image w="100%" src={`https://activity-graph.herokuapp.com/graph?username=aakashindoriya&custom_title=Aakash%20Indoriya%27s%20Contribution%20Graph&theme=gruvbox&bg_color=${theme.bgColor}&hide_border=true&line=d1a01f&point=c58545`} p={2}/>
+        <Flex flexDirection={isMobile?"column":"row"} alignItems="center" justifyContent={"space-around"} p={2}>
+            <Image  src="https://github-readme-stats.vercel.app/api?username=aakashindoriya&show_icons=true&theme=gruvbox&hide_border=true" />
+            <Image  src="https://github-readme-streak-stats.herokuapp.com/?user=aakashindoriya&theme=gruvbox&hide_border=true"/>
+        </Flex>
         </Box>
         </Box>
     )
